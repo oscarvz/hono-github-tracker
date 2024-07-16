3620f1f3246e39c7c94c65662845ce09e2ee754d
oscarvz <oscarvz@users.noreply.github.com>
2024-07-16 15:17:42 +0200

    Query db on `/users` route

--- db/getDb.ts
+++ db/getDb.ts
@@ -5,6 +5,11 @@ import * as schema from "./schema";
 
 let db: NeonHttpDatabase<typeof schema> | undefined;
 
+/**
+ * Returns the database instance. It will create a new instance if it doesn't
+ * exist, or uses the same instance in the current session.
+ * @param databaseUrl
+ */
 export function getDb(databaseUrl: string) {
   if (!db) {
     const sql = neon(databaseUrl);
